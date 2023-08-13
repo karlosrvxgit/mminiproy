e5d0ad46c98d247320a1827af9a3cc960ac79644
Carlos Rojas Vega <karlosrvxx@gmail.com>
2023-08-12 22:49:00 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import "./Card.css";
+import Card from "./Card.jsx";
+import logo from "./img/logo.png";
+import Navbar from "./Navbar";
+import "./Navbar.css"
+
+function App() {
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [cityFilter, setCityFilter] = useState("");
+
+  const getData = async () => {
+    try {
+      const res = await fetch("stays.json");
+      const resJson = await res.json();
+      setData(resJson);
+      setFilteredData(resJson);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const handleCityFilter = (event) => {
+    const cityName = event.target.value.toLowerCase();
+    setCityFilter(cityName);
+
+    const filtered = data.filter((stay) =>
+      stay.city.toLowerCase().includes(cityName)
+    );
+    setFilteredData(filtered);
+  };
+
+  return (
+    <>
+      <nav id="search">
+        <div id="img-logo">
+          <img src={logo} alt="Logo" width="96" />
+        </div>
+      </nav>
+
+      <Navbar cityFilter={cityFilter} handleCityFilter={handleCityFilter} />
+
+      <div id="finland">
+        <div className="stays">
+          <span>Stays in Finland</span>
+        </div>
+        <div className="stays12">
+          <span>{filteredData.length}+ Stays</span>
+        </div>
+      </div>
+
+      <div className="cardcontainer">
+        {filteredData.map((el, index) => (
+          <Card props={el} key={index} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,38 @@
+import "./Navbar.css";
+import React from "react";
+
+const Navbar = ({ cityFilter, handleCityFilter }) => {
+  return (
+    <div className="containernav">
+      <div className="navmain">
+        <form  className="form">
+        <span id="hels">Helsinki,Finland</span>
+        <input
+          className="input"
+          type="text"
+          placeholder="Add guests"
+          value={cityFilter}
+          onChange={handleCityFilter}
+        />
+
+        <button type="submit1" className="submit1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            color="#eb5757"
+            fill="currentColor"
+            className="searchb"
+            // className="bi bi-search"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+          </svg>
+        </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
